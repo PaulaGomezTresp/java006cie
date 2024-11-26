7e96bf78fb88c16db29f24ab2f916df93168f19f
cie <cie@DESKTOP-U2G3M5P>
2024-11-26 12:03:52 +0100

    Holas 5 y 6

--- java02/src/es/curso/principal1.java
+++ java02/src/es/curso/principal1.java
@@ -5,6 +5,8 @@ public class principal1 {
 	public static void main(String[] args) {
 		System.out.println("Hola");
 		System.out.println("Hola4");
+		System.out.println("Hola5");
+		System.out.println("Hola6");
 	}
 
 }
